8c1af68300007d983e4aed30353001a8a1ebc2a8
Cuzin Pro <67429817+cuzinxyz@users.noreply.github.com>
2023-07-10 10:08:29 +0700

    first push

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+       $this->call([StudentSeeder::class]);
+       $this->call([ProductSeeder::class]);
+
+    }
+}
